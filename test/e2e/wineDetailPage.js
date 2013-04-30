cf14fe150304a00a5face66387b38d1702bc2816
Egor Smirnov <egorsmir@gmail.com>
2013-04-30 06:48:34 +0400

    Removed index-e2e.html and reformatted e2e test file

--- test/e2e/wineDetailPage.js
+++ test/e2e/wineDetailPage.js
@@ -7,11 +7,12 @@
  */
 'use strict';
 
-[{path: 'pinot-noir', wine: window.pinotNoir}, {path: 'cabernet', wine: window.cabernet}].forEach(function(obj) {
-  describe('wine detail page', function() {
-    beforeEach(function() {
-      browser().navigateTo('../../index.html#/' + obj.path);
-    });
+[{path: 'pinot-noir', wine: window.pinotNoir},{path: 'cabernet', wine: window.cabernet}].forEach(function (obj) {
+
+    describe('wine detail page', function () {
+        beforeEach(function () {
+            browser().navigateTo('/index.html#/' + obj.path);
+        });
 
 //    TODO: fix this test! (this ia a todo for @bryanchriswhite... bonus point be unto you however, if you can fix it!)
 //  describe('back button', function() {
@@ -27,110 +28,110 @@
 //    });
 //  });
 
-    describe('basic wine info', function() {
-      it('should show the year', function() {
-        expect(element('.year').text()).toBe(obj.wine.year);
-      });
-
-      it('should show the producer name', function() {
-        expect(element('.producer').text()).toBe(obj.wine.producer);
-      });
-
-      it('should show the wine name', function() {
-        expect(element('.name').text()).toBe(obj.wine.name);
-      });
-
-      it('should show the price', function() {
-        expect(element('.price').text()).toBe(obj.wine.price);
-      });
-
-      it('should show the wine properties', function() {
-        expect(element('.properties').query(function(selectedElements, done) {
-          var result = selectedElements.find('li').toArray().every(function(propertyElement) {
-            return obj.wine.properties.some(function(propertyText) {
-              return $(propertyElement).text().match(propertyText);
+        describe('basic wine info', function () {
+            it('should show the year', function () {
+                expect(element('.year').text()).toBe(obj.wine.year);
             });
-          });
 
-          done(null, result);
-        })).toBe(true);
-      });
+            it('should show the producer name', function () {
+                expect(element('.producer').text()).toBe(obj.wine.producer);
+            });
+
+            it('should show the wine name', function () {
+                expect(element('.name').text()).toBe(obj.wine.name);
+            });
+
+            it('should show the price', function () {
+                expect(element('.price').text()).toBe(obj.wine.price);
+            });
+
+            it('should show the wine properties', function () {
+                expect(element('.properties').query(function (selectedElements, done) {
+                    var result = selectedElements.find('li').toArray().every(function (propertyElement) {
+                        return obj.wine.properties.some(function (propertyText) {
+                            return $(propertyElement).text().match(propertyText);
+                        });
+                    });
+
+                    done(null, result);
+                })).toBe(true);
+            });
+        });
+
+        it('should show the wine image', function () {
+            expect(element('.wine-image').attr('src')).toBe(obj.wine.image.url);
+        });
+
+        describe('origin card', function () {
+            describe('style', function () {
+                it('should show the wine style', function () {
+                    expect(element('.style').text()).toBe(obj.wine.style);
+                });
+            });
+
+            describe('grape', function () {
+                it('should show the wine grape', function () {
+                    expect(element('.grape').text()).toBe(obj.wine.grape);
+                });
+            });
+
+            describe('region', function () {
+                it('should show the wine region', function () {
+                    expect(element('.region').text()).toBe(obj.wine.region);
+                });
+            });
+        });
+
+        describe('tasting card', function () {
+            describe('fruit type', function () {
+                it('should show the wine fruit type', function () {
+                    expect(element('.fruit-type-name').text()).toBe(obj.wine.fruitType.name);
+                });
+
+                it('should show the wine fruit type description', function () {
+                    expect(element('.fruit-type-description').text()).toBe(obj.wine.fruitType.description);
+                });
+            });
+
+            describe('body', function () {
+                it('should list the available wine bodies', function () {
+                    expect(element('.body>td').count()).toBe(4);
+                });
+
+                it('should highlight the current wine body', function () {
+                    expect(element('.body .highlight').text()).toBe(obj.wine.body);
+                });
+            });
+
+            describe('dry/sweet', function () {
+                it('should list the available wine dry/sweets', function () {
+                    expect(element('.dry-sweet>td').count()).toBe(4);
+                });
+
+                it('should highlight the current wine dry/sweet', function () {
+                    expect(element('.dry-sweet .highlight').text()).toBe(obj.wine.dry_sweet);
+                });
+            });
+
+            describe('acidity', function () {
+                it('should list the available wine acidities', function () {
+                    expect(element('.acidity>td').count()).toBe(4);
+                });
+
+                it('should highlight the current wine acidity', function () {
+                    expect(element('.acidity .highlight').text()).toBe(obj.wine.acidity);
+                });
+            });
+
+            describe('tannins', function () {
+                it('should list the available wine tannins', function () {
+                    expect(element('.tannins>td').count()).toBe(4);
+                });
+
+                it('should highlight the current wine tannins', function () {
+                    expect(element('.tannins .highlight').text()).toBe(obj.wine.tannins);
+                });
+            });
+        });
     });
-
-    it('should show the wine image', function() {
-      expect(element('.wine-image').attr('src')).toBe(obj.wine.image.url);
-    });
-
-    describe('origin card', function() {
-      describe('style', function() {
-        it('should show the wine style', function() {
-          expect(element('.style').text()).toBe(obj.wine.style);
-        });
-      });
-
-      describe('grape', function() {
-        it('should show the wine grape', function() {
-          expect(element('.grape').text()).toBe(obj.wine.grape);
-        });
-      });
-
-      describe('region', function() {
-        it('should show the wine region', function() {
-          expect(element('.region').text()).toBe(obj.wine.region);
-        });
-      });
-    });
-
-    describe('tasting card', function() {
-      describe('fruit type', function() {
-        it('should show the wine fruit type', function() {
-          expect(element('.fruit-type-name').text()).toBe(obj.wine.fruitType.name);
-        });
-
-        it('should show the wine fruit type description', function() {
-          expect(element('.fruit-type-description').text()).toBe(obj.wine.fruitType.description);
-        });
-      });
-
-      describe('body', function() {
-        it('should list the available wine bodies', function() {
-          expect(element('.body>td').count()).toBe(4);
-        });
-
-        it('should highlight the current wine body', function() {
-          expect(element('.body .highlight').text()).toBe(obj.wine.body);
-        });
-      });
-
-      describe('dry/sweet', function() {
-        it('should list the available wine dry/sweets', function() {
-          expect(element('.dry-sweet>td').count()).toBe(4);
-        });
-
-        it('should highlight the current wine dry/sweet', function() {
-          expect(element('.dry-sweet .highlight').text()).toBe(obj.wine.dry_sweet);
-        });
-      });
-
-      describe('acidity', function() {
-        it('should list the available wine acidities', function() {
-          expect(element('.acidity>td').count()).toBe(4);
-        });
-
-        it('should highlight the current wine acidity', function() {
-          expect(element('.acidity .highlight').text()).toBe(obj.wine.acidity);
-        });
-      });
-
-      describe('tannins', function() {
-        it('should list the available wine tannins', function() {
-          expect(element('.tannins>td').count()).toBe(4);
-        });
-
-        it('should highlight the current wine tannins', function() {
-          expect(element('.tannins .highlight').text()).toBe(obj.wine.tannins);
-        });
-      });
-    });
-  });
 });
